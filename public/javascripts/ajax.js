19cb30c57c934972c8dd630b3dd60458e06e55a0
zuichii <ntc.cordero@gmail.com>
2023-06-07 08:08:56 +0000

    dynamic club profile ajax FIN

--- public/javascripts/ajax.js
+++ public/javascripts/ajax.js
@@ -12,13 +12,16 @@ function getClubProfile() {
       document.getElementById('clubName').innerText = clubData.club_name;
       document.getElementById('clubStats').innerText = '@' + clubData.club_name + ' ' + clubData.num_members + ' MEMBERS ' + clubData.num_posts + ' POSTS';
       document.getElementById('clubLogo').src = "/images/club" + clubId + ".jpg";
+
+      var hero = document.querySelector('.clubHero');
+      hero.style.backgroundImage = 'linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)), url("/images/club' + clubId + '.jpg")';
     }
   };
 
   xhttp.open('GET', '/club_profile?id=' + clubId, true);
   xhttp.send();
 }
-
+// call the function after page loads
 window.addEventListener('DOMContentLoaded', getClubProfile);
 
 
